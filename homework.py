88dc63519f21170d64629a2b4847d97e9fe57748
nvmaxim <m.novikov2050@ya.ru>
2025-07-22 01:28:16 +0300

    dz

--- homework.py
+++ homework.py
@@ -1,6 +1,10 @@
-x1, y1, x2, y2 = int(input()), int(input()), int(input()), int(input())
+x1, y1 = int(input()), int(input())
+x2, y2 = int(input()), int(input())
 
-if abs(x1 - x2) == abs(y1 - y2):
+dx = abs(x1 - x2)
+dy = abs(y1 - y2)
+
+if (dx == 1 and dy == 2) or (dx == 2 and dy == 1):
     print("YES")
 else:
     print("NO")
